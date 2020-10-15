356673a1e7c454a2dd66c1bc120bf7ac3b64c5f4
Desiree Caldera <desireecaldera@Desirees-MacBook-Pro.local>
2020-10-14 17:53:15 -0700

    Class Work 10/14

--- src/parse.c
+++ src/parse.c
@@ -65,49 +65,239 @@ NODE *statement()
 
 NODE *assignStmt(TOKEN **currToken)
 {
-    // TODO
-    return NULL;
+    NODE *node = calloc(sizeof(NODE), 1);
+    node->type = ASSIGN_STMT_NODE;
+
+    node->leftChild = ident(currToken);
+
+    //verify that next token is is a valid sign (=)
+
+    *currToken = getNextToken( currToken );
+    if( (*currToken)->type !=ASSIGNMENT_TOKEN ){
+        error( "Expected assigment token in assignment statement");
+
+    }
+    *currToken = getNextToken(currToken);
+
+    node->rightChild = expr( currToken );
+
+    //now read next token
+    *currToken = getNextToken(currToken);
+    //currTok should be a semicol now
+
+    if( (*currToken)->type !=SEMICOLON_TOKEN){
+        error( "Expected assigment token in assignment statement");
+
+    }
+
+    freeToken( currToken );
+
+
+    // TODO(done)
+    return node;
 }
 
 NODE *repeatStmt(TOKEN **currToken)
 {
-    // TODO
-    return NULL;
+    //repeat ( <expr> ) <statement>
+    NODE *node = calloc( sizeof( NODE ), 1);
+    node->type = REPEAT_STMT_NODE;
+
+    *currToken = getNextToken( currToken );
+    if( (*currToken)->type != REPEAT_TOKEN ){
+        error( "Expected repeat keyword");
+    }
+
+    *currToken = getNextToken( currToken );
+    if( (*currToken)->type != LPAREN_TOKEN ){
+        error( "Expected left parenthesis");
+    }
+
+    *currToken = getNextToken( currToken );
+    node->leftChild = expr( currToken );
+
+    *currToken = getNextToken( currToken );
+    if( (*currToken)->type != RPAREN_TOKEN ){
+        error( "Expected right parenthesis");
+    }
+
+    *currToken = getNextToken( currToken );
+    node->rightChild = statement();
+
+    //do i need to free here? yes because we do not pass into a function that frees it
+    //nor do we call getNextToken AGAIN to free currToken
+    freeToken( currToken);
+
+    // TODO(done double check)
+    return node;
 }
 
 NODE *printStmt(TOKEN **currToken)
 {
-    // TODO
-    return NULL;
+    //print ( <expr> );
+    NODE *node = calloc( sizeof( NODE ), 1 );
+    node->type = PRINT_STMT_NODE;
+
+    *currToken = getNextToken( currToken );
+    if( (*currToken)->type != PRINT_TOKEN ){
+        error( "Expected repeat keyword");
+    }
+
+    *currToken = getNextToken( currToken );
+    if( (*currToken)->type != LPAREN_TOKEN ){
+        error( "Expected left parenthesis");
+    }
+
+    *currToken = getNextToken( currToken );
+    node->leftChild = expr( currToken );
+
+    *currToken = getNextToken( currToken );
+    if( (*currToken)->type != RPAREN_TOKEN ){
+        error( "Expected reft parenthesis");
+    }
+
+    *currToken = getNextToken( currToken );
+    if( (*currToken)->type != SEMICOLON_TOKEN ){
+        error( "Expected a semicolon" );
+    }
+
+
+    freeToken( currToken );
+
+    // TODO(done double check)
+    return node;
 }
 
 NODE *expr(TOKEN **currToken)
 {
-    // TODO
-    return NULL;
+    //<term> | <term> <addop> <expr>
+    NODE *node = calloc( sizeof( NODE ), 1 );
+    node->type = EXPR_NODE;
+
+    //<term>
+    *currToken = getNextToken( currToken );
+    node->leftChild = term( currToken );
+
+    *currToken = getNextToken( currToken );
+
+    //check for <addop>
+    if( (*currToken)->type != ADD_OP_TOKEN ){
+        ungetToken( currToken );
+    } else{
+        //double check this one vvv
+        node->data.op =  *currToken;
+
+        //<expr>
+        *currToken = getNextToken( currToken );
+        node->rightChild = expr( currToken );
+
+    }
+
+    // TODO(done double check)
+    return node;
 }
 
 NODE *term(TOKEN **currToken)
 {
-    // TODO
-    return NULL;
+    //<factor> | <factor> <multop> <term>
+
+    NODE *node = calloc( sizeof( NODE ), 1 );
+    node->type = TERM_NODE;
+
+    //<factor>
+    *currToken = getNextToken( currToken );
+    node->leftChild = factor( currToken );
+
+    *currToken = getNextToken( currToken );
+
+    //checking for <multop>
+    if( (*currToken)->type != MULT_OP_TOKEN ){
+        ungetToken( currToken );
+    } else{
+        node->data.op = *currToken;
+
+        //<term>
+        *currToken = getNextToken( currToken );
+        node->rightChild = term( currToken );
+    }
+
+
+    // TODO(done double check)
+    return node;
 }
 
 NODE *factor(TOKEN **currToken)
 {
-    // TODO
-    return NULL;
+   // <id> | <number> | <addop> <factor> | ( <expr> )
+    NODE *node = calloc( sizeof( NODE ), 1 );
+    node->type = FACTOR_NODE;
+
+
+    *currToken = getNextToken( currToken );
+    //<id>
+    if( (*currToken)->type == IDENT_TOKEN ){
+        node->leftChild = ident( currToken );
+
+        //<number>
+    } else if( (*currToken)->type == NO_TOKEN_TYPE ){
+        node->leftChild = number( currToken);
+
+        //<addop>
+    } else if( (*currToken)->type == ADD_OP_TOKEN ){
+        node->data.op = *currToken;
+
+        //<factor>
+        *currToken = getNextToken( currToken );
+        node->rightChild = factor( currToken );
+
+
+        // (<expr>)
+    }else if( (*currToken)->type == LPAREN_TOKEN ){
+        node->leftChild = expr( currToken);
+
+        *currToken = getNextToken( currToken );
+        if( (*currToken)->type != RPAREN_TOKEN ){
+            error( "Expected reft parenthesis");
+        }
+        freeToken( currToken );
+
+    } else{
+        error( "Not a valid factor statement");
+    }
+
+
+    // TODO(done double check)
+    return node;
 }
 
 NODE *ident(TOKEN **currToken)
 {
+    // <letter> | <id> <letter> | <id> <digit>
+    NODE *node  = calloc( sizeof( NODE ), 1 );
+    node->type = IDENT_NODE;
+
+    //we need to strcpy into ident
+
     // TODO
     return NULL;
 }
 
 NODE *number(TOKEN **currToken)
 {
-    // TODO
+    //<int> | <float>
+    NODE *node = calloc( sizeof( NODE ), 1 );
+    node->type = NUMBER_NODE;
+
+    *currToken = getNextToken( currToken );
+    if( (*currToken)->type == INT_TOKEN ){
+        node->data.number.type = INT_TYPE;
+        node->data.number.value.integral = strtol(*currToken, NULL, 10 );
+    } else if ((*currToken)->type == FLOAT_TOKEN ){
+        node->data.number.type = FLOAT_TYPE;
+        node->data.number.value.floating_point = strtod(*currToken, NULL);
+    }
+
+    // TODO(done but double check)
     return NULL;
 }
 
